b6f977c242bb876e80cac1d1d51dcde97027a69f
TheThumbFan <st7394767@stud.duikt.edu.ua>
2025-10-21 12:01:58 +0300

    Update test_task5.py

--- test/test_task5.py
+++ test/test_task5.py
@@ -1,10 +1,15 @@
 import unittest
-import Task5
+from Task5 import PasswordGenerator
 
-class TestTask5(unittest.TestCase):
-    def test_exists(self):
-        self.assertTrue(hasattr(Task5, "__doc__"))
+class TestPasswordGenerator(unittest.TestCase):
+    def test_password_length(self):
+        gen = PasswordGenerator(level="Середній", length=10)
+        password = gen.generate()
+        self.assertEqual(len(password), 10)
+
+    def test_invalid_level(self):
+        with self.assertRaises(ValueError):
+            PasswordGenerator(level="Невідомий").generate()
 
 if __name__ == "__main__":
     unittest.main()
-  
